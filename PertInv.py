b4daae462148aa9ed8ade4d3a8a6c3d52b45b584
Bednarke <bednarke@uw.edu>
2018-04-29 14:01:44 -0700

    test conf

--- PertInv.py
+++ PertInv.py
@@ -19,7 +19,7 @@ info = mne.io.read_info(raw_fname)
 ########################################################################
 sphere = mne.make_sphere_model(r0=(0., 0., 0.), head_radius=None) # Use this for all testing
 
-pos = dict(rr=[[0, 0, 0], [.05, .05, .05]], nn=[[0, 0, 1], [0, 0, 1]])  # in head coords
+pos = dict(rr=[[.05, .05, .05]], nn=[[0, 0, 1]])
 pos['rr'] = mne.transforms.apply_trans(head_mri_t, pos['rr'])  # invert back to mri
 pos['nn'] = mne.transforms.apply_trans(head_mri_t, pos['nn'])
 src = mne.setup_volume_source_space(subject=subject, pos=pos, mri=None,
@@ -29,26 +29,10 @@ src = mne.setup_volume_source_space(subject=subject, pos=pos, mri=None,
                                     add_interpolator=True, verbose=None)
 # print src
 print('sources in MRI')
-print(src[0]['rr'])
-srchead = src.copy()  # Now Transform this to head coords
-
-for i in xrange(len(src)):
-    srchead[i]['rr'] = mne.transforms.apply_trans(mri_head_t, src[i]['rr'])
-    srchead[i]['nn'] = mne.transforms.apply_trans(mri_head_t, src[i]['rr'])
-print('sources back in Head')
-print(srchead[0]['rr'])
 fwd = mne.make_forward_solution(raw_fname, trans=trans, src=src, bem=sphere,
                                 meg=True, eeg=False, mindist=1.0, n_jobs=1)
-SourceLocations1 = fwd['src'][0]['rr']
-fixedtruth = mne.forward.is_fixed_orient(fwd)
-print(fixedtruth)  # indeed not fixed
-print(SourceLocations1)  # This indicates our fwd is in head coords!
-
 fwd_fixed = mne.convert_forward_solution(fwd, surf_ori=True, force_fixed=True,
                                          use_cps=True)
-fixedtruth = mne.forward.is_fixed_orient(fwd_fixed
-                                         )
-print(fixedtruth)  # indeed not fixed
 leadfield = fwd_fixed['sol']['data']
 SourceLocations2 = fwd_fixed['src'][0]['rr']
 print(SourceLocations2)
@@ -62,7 +46,7 @@ mne.write_forward_solution(fixed_fwd_fname, fwd_fixed, overwrite=True, verbose=N
 # forward operator to a source estimate composed of the identity operator:
 n_dipoles = leadfield.shape[1]
 vertices = [src_hemi['vertno'] for src_hemi in fwd_fixed['src']]
-stc = mne.VolSourceEstimate(1e-9 * np.eye(n_dipoles), vertices, tmin=0., tstep=1)
+stc = mne.VolSourceEstimate(1e-1 * np.eye(n_dipoles), vertices, tmin=0., tstep=1)
 cov = mne.read_cov(cov_fname)
 nave = 10000  # simulate average of 100 epochs
 evoked = mne.simulation.simulate_evoked(fwd_fixed, stc, info, cov, nave=nave, use_cps=True,
@@ -78,8 +62,8 @@ mne.write_evokeds(sim_ave_fname, evoked)
 ###############################################################################
 pos = dict(rr=[[.05, .05, .05]], nn=[[0, 0, 1]])
 times = [1]
-dip = mne.Dipole(times, pos['rr'], [1e-5], pos['nn'], [1], name=None, conf=None, khi2=None, nfree=None)
-fwd_dip, stc_dip = mne.forward.make_forward_dipole(dip, sphere, info, trans)
+dip = mne.Dipole(times, pos['rr'], [1e-3], pos['nn'], [1], name=None, conf=None, khi2=None, nfree=None)
+fwd_dip, stc_dip = mne.forward.make_forward_dipole(dip, sphere, evoked.info, trans)
 fwd_dip_fixed = mne.convert_forward_solution(fwd_dip, surf_ori=True, force_fixed=True,
                                              use_cps=True)
 leadfield_dip = fwd_dip_fixed['sol']['data']
@@ -110,6 +94,6 @@ dip_fit.pos[0, 2] = zsum / 10
 
 # print('Dipole fit at location', dip_fit.pos)
 # dip_fit.plot_locations(trans, 'sample', subjects_dir, coord_frame='head', mode='orthoview')
-dip_fit = mne.fit_dipole(evoked_dip, cov_fname, sphere, trans)[0]
+dip_fit = mne.fit_dipole(evoked, cov_fname, sphere, trans)[0]
 print('Dipole fit at location', dip_fit.pos)
 
---
+++ sim_funcs.py
@@ -0,0 +1,6 @@
+# House funcs here so PertInv is just scripting
+
+def compute_fwds_stc():
+    return unpertfwd, pertfwd, stc
+
+
