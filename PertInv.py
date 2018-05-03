03cafd11c24f9ee4695555556a63e6aa7ef33987
Bednarke <bednarke@uw.edu>
2018-05-02 18:58:50 -0700

    Need to define PC space

--- PertInv.py
+++ PertInv.py
@@ -1,5 +1,6 @@
 import mne
 from mne.datasets import sample
+import numpy as np
 from sim_funcs import compute_fwds_stc_with_make_forward_dipole, compute_fwds_stc
 
 local_data_path = 'C:\MEG\Local_mne_data'
@@ -16,21 +17,73 @@ info = mne.io.read_info(raw_fname)
 # Setup our sources, bem
 ########################################################################
 sphere = mne.make_sphere_model(r0=(0., 0., 0.), head_radius=None)  # Use this for all testing
-position = dict(rr=[[.05, .05, .05]], nn=[[0, 0, 1]])
-times = [1]
-coils = 'placeholder'
-fwd_fixed, fwd_pert_fixed, stc = compute_fwds_stc(position, coils, sphere)
 
-evoked = mne.simulation.simulate_evoked(fwd_fixed, stc, info, cov, use_cps=True,
-                                        iir_filter=None)
-evoked_pert = mne.simulation.simulate_evoked(fwd_pert_fixed, stc, info, cov, use_cps=True,
-                                             iir_filter=None)
+coils = 'placeholder'
+testsources = dict(rr=[], nn=[])
+for i in range(0, 70):
+    source = [0, 0, .001*i]
+    normal = [.5, .5, 0]
+    testsources['rr'].append(source)
+    testsources['nn'].append(normal)
+
+position = dict(rr=[0], nn=[0])
+data = np.zeros((10, 22))
+print testsources['rr'][69][1]
+for i in range(60, 70):
+    position['rr'][0] = testsources['rr'][i]
+    position['nn'][0] = testsources['nn'][i]
+    fwd_fixed, fwd_pert_fixed, stc = compute_fwds_stc(position, coils, sphere)
+    evoked = mne.simulation.simulate_evoked(fwd_fixed, stc, info, cov, use_cps=True,
+                                            iir_filter=None)
+    evoked_pert = mne.simulation.simulate_evoked(fwd_pert_fixed, stc, info, cov, use_cps=True,
+                                                 iir_filter=None)
+    xsum, ysum, zsum = 0, 0, 0
+
+    dip_fit_long = mne.fit_dipole(evoked, cov_fname, sphere, trans)[0]
+    dip_fit_pert = mne.fit_dipole(evoked, cov_fname, sphere, trans)[0]
+
+    for x in range(0, 5):
+        dip_fit = mne.fit_dipole(evoked, cov_fname, sphere, trans)[0]
+        xsum += dip_fit.pos[0, 0]
+        ysum += dip_fit.pos[0, 1]
+        zsum += dip_fit.pos[0, 2]
+
+    dip_fit_long.pos[0, 0] = xsum / 5
+    dip_fit_long.pos[0, 1] = ysum / 5
+    dip_fit_long.pos[0, 2] = zsum / 5
+
+    xsum, ysum, zsum = 0, 0, 0
+    for x in range(0, 5):
+        dip_fit = mne.fit_dipole(evoked_pert, cov_fname, sphere, trans)[0]
+        xsum += dip_fit.pos[0, 0]
+        ysum += dip_fit.pos[0, 1]
+        zsum += dip_fit.pos[0, 2]
+
+    dip_fit_pert.pos[0, 0] = xsum / 5
+    dip_fit_pert.pos[0, 1] = ysum / 5
+    dip_fit_pert.pos[0, 2] = zsum / 5
+
+    print dip_fit_long.pos[0][2]
+    dip_fit_pert = mne.fit_dipole(evoked_pert, cov_fname, sphere, trans)[0]
+    del fwd_fixed, fwd_pert_fixed, evoked, evoked_pert
+    i = i - 60
+    for j in range(0, 3):
+        data[i, 1+j] = testsources['rr'][i][j]
+        data[i, 4 + j] = dip_fit_long.pos[0][j]
+        data[i, 7 + j] = dip_fit_long.ori[0][j]
+        data[i, 10 + j] = dip_fit_pert.pos[0][j]
+        data[i, 13+j] = dip_fit_pert.ori[0][j]
+    i = i + 60
+    print('Long fit, short fit:', dip_fit_long.pos, dip_fit_pert.pos)
+data_fname = local_data_path + '/rand_pert_data.csv'
+np.savetxt(data_fname, data, delimiter=",")
+
+
+
 
 ###############################################################################
 
-dip_fit_long = mne.fit_dipole(evoked, cov_fname, sphere, trans)[0]
-dip_fit_pert = mne.fit_dipole(evoked_pert, cov_fname, sphere, trans)[0]
-print('Long fit, short fit:', dip_fit_long.pos, dip_fit_pert.pos)
 
-#
+
+
 
