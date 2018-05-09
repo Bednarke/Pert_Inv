85682d5a6f243c83c891a01b41e88aa0e4f39cd5
Bednarke <bednarke@uw.edu>
2018-05-09 10:59:05 -0700

    more data

--- PertInv.py
+++ PertInv.py
@@ -12,16 +12,16 @@ perts = dict(max_percent_imb=10, variance_imb=0, preferred_side_imb=1,
 sign = perts['preferred_side_imb']
 max_imbalance = perts['max_percent_imb']
 max_shift = perts['max_translation']
-local_data_path = 'C:\Pert_Inv\Local_mne_data\side%s' % (sign,)
+local_data_path = 'C:\Pert_Inv\Local_mne_data\small_imbalance\side%s' % (sign,)  # 'C:\Pert_Inv\Local_mne_data\side%s' % (sign,)
 min_rad, max_rad = 0, 90
 
 big_data = np.zeros((max_shift + 1, max_imbalance + 1, max_rad - min_rad + 1, 22))
-nn = [0, 1, 1]
+nn = [1, 0, 1]
 sourcenorm = [0, 1, 0] / np.linalg.norm([0, 1, 0])
 for l in range(0, max_shift + 1):
     perts['max_translation'] = l
     for k in range(0, max_imbalance + 1):
-        perts['max_percent_imb'] = k
+        perts['max_percent_imb'] = .1*k
         dip_fit_long, dip_fit_pert, testsources = fit_dips(min_rad, max_rad, nn, sphere, perts, sourcenorm)
         data = np.zeros((max_rad - min_rad + 1, 22))
         for i in range(0, max_rad):
@@ -31,7 +31,7 @@ for l in range(0, max_shift + 1):
                 data[i, 7 + j] = round(dip_fit_long.ori[i][j], 6)
                 data[i, 10 + j] = round(dip_fit_pert.pos[i][j], 6)
                 data[i, 13 + j] = round(dip_fit_pert.ori[i][j], 6)
-                data[i, 19] = round(dip_fit_pert.pos[i][0], 6)
+                data[i, 19] = round(dip_fit_pert.pos[i][1], 6)
                 data[i, 20] = round(dip_fit_long.gof[i], 6)
                 data[i, 21] = round(dip_fit_pert.gof[i], 6)
         big_data[l, k] = data
